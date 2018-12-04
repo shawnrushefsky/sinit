69bd75fd86aae493140d3caa520976947d23f8c4
Shawn Rushefsky <shawn.rushefsky@gmail.com>
2018-12-04 11:00:52 -0600

    basic command line parser

---
+++ cmd/sinit/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+func main() {
+	var (
+		projName string
+		stack    string
+	)
+
+	const (
+		projNameDefault = "new-project"
+		projNameUsage   = "The name of your new project"
+	)
+	flag.StringVar(&projName, "name", projNameDefault, projNameUsage)
+	flag.StringVar(&projName, "n", projNameDefault, projNameUsage+" (shorthand)")
+
+	const (
+		stackDefault = "node"
+		stackUsage   = "The project stack you want to create. e.g. node"
+	)
+	flag.StringVar(&stack, "stack", stackDefault, stackUsage)
+	flag.StringVar(&stack, "s", stackDefault, stackUsage+" (shorthand)")
+
+	flag.Parse()
+
+	fmt.Println("Name: " + projName)
+	fmt.Println("Stack: " + stack)
+}
