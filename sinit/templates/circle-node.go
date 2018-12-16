f98a949421ca7490d0bceba1de5d912e9e44a61f
Shawn Rushefsky <shawn.rushefsky@gmail.com>
2018-12-16 15:05:20 -0600

    package.json and circle-node.yml

---
+++ sinit/templates/circle-node.go
@@ -0,0 +1,44 @@
+package templates
+
+import "text/template"
+
+// CircleNode returns the readme template
+func CircleNode() (t *template.Template) {
+	const raw = `version: 2
+	jobs:
+		build:
+			docker:
+				- image: circleci/node:{{.NodeVersion}}-stretch
+	
+			steps:
+				- checkout
+	
+				# Download and cache dependencies
+				- restore_cache:
+						keys:
+						- v1-dependencies-{{"{{"}} checksum "package.json" {{"}}"}}
+						# fallback to using the latest cache if no exact match is found
+						- v1-dependencies-
+	
+				- run: 
+						name: Install Dependencies
+						command: npm install
+	
+				- save_cache:
+						paths:
+							- node_modules
+						key: v1-dependencies-{{"{{"}} checksum "package.json" {{"}}"}}
+					
+				# run tests!
+				- run: 
+						name: Run Tests
+						command: npm test
+	`
+
+	t, err := template.New("circle-node.yml").Parse(raw)
+	if err != nil {
+		panic(err)
+	}
+
+	return
+}
